98c1a481b15d672919a05c913bfb4b63b456dd1c
wsilverio <wendeurick@gmail.com>
2015-03-13 11:35:29 -0300

    add rects

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <stdio.h>
 #include <opencv2/opencv.hpp> // openCV
+#include <vector>
 
 #include <common/mavlink.h>     // Protocolo Mavlink
 #include "libs/serial_port.h"        // Serial_Port
@@ -58,8 +60,11 @@ int main(int argc, char const *argv[])
 
     signal(SIGINT, Quit_Handler);
 
-    #define windowName "OPTICAL FLOW"
-    cv::namedWindow(windowName, CV_WINDOW_AUTOSIZE);
+    #define ACCUM_POS "Accumulated Position"
+    cv::namedWindow(ACCUM_POS, CV_WINDOW_AUTOSIZE);
+
+    #define CURRENT_POS "Current Position"
+    cv::namedWindow(CURRENT_POS, CV_WINDOW_AUTOSIZE);    
 
     serial_port.start();
     px4flow.start();
@@ -69,7 +74,24 @@ int main(int argc, char const *argv[])
     
     double delta_time, X_accum = 0, Y_accum = 0;
 
-    cv::Mat imagemPlot = cv::Mat(cv::Size(600, 600), CV_8UC3, cv::Scalar(66, 59, 55));
+    cv::Scalar bgColor(66, 59, 55);
+    cv::Scalar fillColor(242, 242, 242);
+
+    cv::Mat accumPosImg = cv::Mat(cv::Size(600, 600), CV_8UC3, bgColor);
+
+    #define HEIGHT 300
+    #define MARGEM 30
+    #define TAM (HEIGHT - 2*MARGEM)
+    cv::Mat currentPosImg = cv::Mat(cv::Size(4*MARGEM + 3*TAM, HEIGHT), CV_8UC3, bgColor);
+
+    char *frase[3] = {"current position", "flow vector", "ground distance"};
+    std::vector<cv::Rect> rects(3);
+
+    for (int i = 0; i < rects.size(); ++i){
+        rects[i] = cv::Rect((i+1)*MARGEM + i*TAM - 1, MARGEM - 1, TAM, TAM);
+        cv::rectangle(currentPosImg, rects[i], fillColor, 1, 8, 0);
+        putText(currentPosImg, frase[i], cv::Point(rects[i].x, HEIGHT - MARGEM/2.0), cv::FONT_HERSHEY_SIMPLEX, 0.4, fillColor, 1, CV_AA);
+    }
 
     while(1){
 
@@ -114,8 +136,38 @@ int main(int argc, char const *argv[])
         //     "y pos: %f\n\n",
         //     X_accum, Y_accum);
 
-        cv::circle(imagemPlot, cv::Point( Map( Constrain(X_accum,-5,5), -5, 5, 0, imagemPlot.cols), Map( Constrain(Y_accum,-5,5), -5, 5, 0, imagemPlot.rows)), 1, cv::Scalar(242, 242, 242), -1, CV_AA, 0);
+        cv::Point2f pos(
+            Map( Constrain(X_accum,-5,5), -5, 5, 0, accumPosImg.cols),
+            Map( Constrain(Y_accum,-5,5), -5, 5, 0, accumPosImg.rows)
+            );
 
+        cv::circle(accumPosImg, pos, 1, fillColor, -1, CV_AA, 0);
+
+        currentPosImg = cv::Mat(cv::Size(4*MARGEM + 3*TAM, HEIGHT), CV_8UC3, bgColor);
+        for (int i = 0; i < rects.size(); ++i){
+            cv::rectangle(currentPosImg, rects[i], fillColor, 1, 8, 0);
+            putText(currentPosImg, frase[i], cv::Point(rects[i].x, HEIGHT - MARGEM/2.0), cv::FONT_HERSHEY_SIMPLEX, 0.4, fillColor, 1, CV_AA);
+        }
+
+        pos = cv::Point(
+                Map( pos.x, 0, accumPosImg.cols, rects[0].x, rects[0].x + TAM),
+                Map( pos.y, 0, accumPosImg.rows, rects[0].y, rects[0].y + TAM)
+                );
+
+        cv::circle( currentPosImg,
+                    cv::Point(  Constrain(pos.x, rects[0].x + 4, rects[0].x + TAM - 4),
+                                Constrain(pos.y, rects[0].y + 4, rects[0].y + TAM - 4)),
+                    3, fillColor, 1, CV_AA, 0);
+
+        #define SCALE 0.25
+        cv::Point2f center(rects[1].x + TAM/2.0, rects[1].y + TAM/2.0);
+        cv::line(currentPosImg, center, center - cv::Point2f(flow_x * SCALE, flow_y * SCALE), fillColor, 1, CV_AA, 0);
+        cv::line(currentPosImg, center, center - cv::Point2f(flow_x * SCALE, 0), cv::Scalar(52, 41, 166), 1, CV_AA, 0);
+        cv::line(currentPosImg, center, center - cv::Point2f(0, flow_y * SCALE), cv::Scalar(78, 104, 18), 1, CV_AA, 0);
+
+        #define GND_DIST_MAX 5
+        float distConstr = Constrain( Map(ground_distance, 0, GND_DIST_MAX, 0, TAM), 0, TAM);
+        cv::rectangle(currentPosImg, cv::Rect(rects[2].x + TAM/3.0, rects[2].y + TAM - distConstr, TAM/3.0, distConstr), fillColor, -1, 8, 0);
 
         // do{
             char c = (char) cvWaitKey(1); // 1ms
@@ -123,10 +175,11 @@ int main(int argc, char const *argv[])
 
         // }while(not px4flow.img.rows && not px4flow.img.cols);
 
-        // px4flow.img.copyTo(imagemPlot);
+        // px4flow.img.copyTo(imagePlot);
 
         // // Exibe a imagem
-        cv::imshow(windowName, imagemPlot);
+        cv::imshow(ACCUM_POS, accumPosImg);
+        cv::imshow(CURRENT_POS, currentPosImg);
     }
 
     return 0;
